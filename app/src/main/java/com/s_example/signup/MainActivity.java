03e1e42096e2491e054d62bdf71324bf43e5583f
jani <jani.m.h.yar@gmail.com>
2018-08-18 17:01:08 +0430

    addDetails

--- app/src/main/java/com/s_example/signup/MainActivity.java
+++ app/src/main/java/com/s_example/signup/MainActivity.java
@@ -3,14 +3,20 @@ package com.s_example.signup;
 import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.transition.TransitionManager;
+import android.view.Gravity;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
+    ViewGroup viewGroup;
     TextView txt;
     EditText editLog;
     String show;
@@ -25,8 +31,18 @@ public class MainActivity extends AppCompatActivity {
         editLog = (EditText)findViewById(R.id.user);
         final EditText editPass = (EditText)findViewById(R.id.password);
 
+        final ImageView btn = (ImageView) findViewById(R.id.btnAnimation);
+        viewGroup = (ViewGroup)findViewById(R.id.group);
 
 
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                TransitionManager.beginDelayedTransition(viewGroup);
+                LinearLayout.LayoutParams l = (LinearLayout.LayoutParams) btn.getLayoutParams();
+                l.gravity= Gravity.LEFT;
+            }
+        });
 
 
         btn_create.setOnClickListener(new View.OnClickListener() {
